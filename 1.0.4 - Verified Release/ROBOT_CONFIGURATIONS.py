be9e74423b9fea337744d6dcc25df2adef1de265
PlannerX-GCS <plannerxgcs@gmail.com>
2025-02-06 10:16:14 +0530

    Update ROBOT_CONFIGURATIONS.py

--- 1.0.4 - Verified Release/ROBOT_CONFIGURATIONS.py
+++ 1.0.4 - Verified Release/ROBOT_CONFIGURATIONS.py
@@ -13,11 +13,22 @@ def robot_category():
 
 
 def robot_node_mapping():
-    # Initialize an empty dictionary
-    mapped_port_number = OrderedDict()
-    port_number = []
-    controllers = []
-    controlled = []
+
+    LEFT_IR = []
+    RIGHT_IR = []
+    LEFT_MOTORS = []
+    RIGHT_MOTORS = []
+    LEFT_MOTOR_SPEED = []
+    RIGHT_MOTOR_SPEED = []
+    ECHO = []
+    TRIGGER = []
+    SERVO = []
+    RGB_RED = []
+    RGB_BLUE = []
+    RGB_GREEN = []
+    RGB_SINGLE = []
+    LDR = []
+    
     reference = {"A":7, "B":8, "C":9, "D":10, "E":11, "F":12, "G":13, "H":2, "I":3, "J":4, "K":5, "L":6, "M":22}
 
     # Open the file and read lines
@@ -28,53 +39,41 @@ def robot_node_mapping():
     for line in lines:
         values = line.strip().split(',')
         for value in values:
-            if "RC_3" in str(value):
-                port_number.append("THR_"+ str(reference[str(value)[-1:]]))
-            elif str(value)[-1:] in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"] and "RC_3" not in str(value):
-                port_number.append(reference[str(value)[-1:]])
-            elif str(value) == "None":
-                port_number.append("NA")
-            elif value == "":
-                port_number.append(int(0))
-            else:
-                port_number.append(int(value))
-        mapped_port_number["FLM"] = ((port_number[0]), (port_number[1]))
-        mapped_port_number["RLM"] = ((port_number[2]), (port_number[3]))
-        mapped_port_number["RRM"] = ((port_number[4]), (port_number[5]))
-        mapped_port_number["FRM"] = ((port_number[6]), (port_number[7]))
-        mapped_port_number["Steer 1"] = ((port_number[8]), (port_number[9]), (port_number[10]))
-        mapped_port_number["Steer 2"] = ((port_number[11]), (port_number[12]), (port_number[13]))
-        mapped_port_number["Wrist"] = ((port_number[14]), (port_number[15]), (port_number[16]))
-        mapped_port_number["Elbow"] = ((port_number[17]), (port_number[18]), (port_number[19]))
-        mapped_port_number["Shoulder_Z"] = ((port_number[20]), (port_number[21]), (port_number[22]))
-        mapped_port_number["Shoulder_X"] = ((port_number[23]), (port_number[24]), (port_number[25]))
-        mapped_port_number["Gripper_1"] = ((port_number[26]), (port_number[27]), (port_number[28]))
-        mapped_port_number["Gripper_2"] = ((port_number[29]), (port_number[30]), (port_number[31]))
-        mapped_port_number["FB_Controller"] = ((port_number[32]))
-        mapped_port_number["Axis_Turn_Controller"] = ((port_number[33]))
-        mapped_port_number["Slide_Controller"] = ((port_number[34]))
-        mapped_port_number["Steer_1_Controller"] = ((port_number[35]))
-        mapped_port_number["Steer_2_Controller"] = ((port_number[36]))
-        mapped_port_number["Shoulder_Z_Controller"] = ((port_number[37]))
-        mapped_port_number["Shoulder_X_Controller"] = ((port_number[38]))
-        mapped_port_number["Elbow_Controller"] = ((port_number[39]))
-        mapped_port_number["Wrist_Controller"] = ((port_number[40]))
-        mapped_port_number["Gripper_1_Controller"] = ((port_number[41]))
-        mapped_port_number["Gripper_2_Controller"] = ((port_number[42]))
-        mapped_port_number["Speed_Controller_1"] = ((port_number[43]))
-        mapped_port_number["Speed_Controller_2"] = ((port_number[44]))
-        mapped_port_number["IR_Left"] = ((port_number[45]))
-        mapped_port_number["IR_Right"] = ((port_number[46]))
-        mapped_port_number["Trigger"] = ((port_number[47]))
-        mapped_port_number["Echo"] = ((port_number[48]))
+            if "No Connection" in str(value):
+                pass
+            elif str(value)[-1:] in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]:
+                port_number = reference[str(value)[-1:]]
+            
+                if "Left_IR" in value:
+                    LEFT_IR.append(port_number)
+                elif "Right_IR" in value:
+                    RIGHT_IR.append(port_number)
+                elif "Left_DC" in value:
+                    LEFT_MOTORS.append(port_number)
+                elif "Right_DC" in value:
+                    RIGHT_MOTORS.append(port_number)
+                elif "Left_Motor_Speed" in value:
+                    LEFT_MOTOR_SPEED.append(port_number)
+                elif "Right_Motor_Speed" in value:
+                    RIGHT_MOTOR_SPEED.append(port_number)
+                elif "Echo" in value:
+                    ECHO.append(port_number)
+                elif "Trigger" in value:
+                    TRIGGER.append(port_number)
+                elif "Servo" in value:
+                    SERVO.append(port_number)
+                elif "RGB_Red" in value:
+                    RGB_RED.append(port_number)
+                elif "RGB_Blue" in value:
+                    RGB_BLUE.append(port_number)
+                elif "RGB_Green" in value:
+                    RGB_GREEN.append(port_number)
+                elif "RGB_Single" in value:
+                    RGB_SINGLE.append(port_number)
+                elif "LDR" in value:
+                    LDR.append(port_number)
 
-        
-        
-        controllers += [port_number[32], port_number[33], port_number[34], port_number[35], port_number[36], port_number[37], port_number[38], port_number[39], port_number[40], port_number[41], port_number[42], port_number[45], port_number[46], port_number[48]]
-        controlled += [port_number[i] for i in range(32)]
-        controlled += [(port_number[43]), (port_number[44]),  port_number[47]]
-
-    return mapped_port_number, controllers, controlled
+    return LEFT_IR, RIGHT_IR, LEFT_MOTORS, RIGHT_MOTORS, LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED, ECHO, TRIGGER, SERVO, RGB_RED, RGB_BLUE, RGB_GREEN, RGB_SINGLE, LDR
 
 
 def mode_selection():
