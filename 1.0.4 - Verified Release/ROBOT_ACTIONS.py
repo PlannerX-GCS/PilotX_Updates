a28004078ba1838a978f7e0c99d3f01a751046ba
PlannerX-GCS <plannerxgcs@gmail.com>
2025-02-06 11:32:05 +0530

    Update ROBOT_ACTIONS.py

--- 1.0.4 - Verified Release/ROBOT_ACTIONS.py
+++ 1.0.4 - Verified Release/ROBOT_ACTIONS.py
@@ -49,170 +49,100 @@ def robot_forward(left_speed, right_speed):
 
 
 def robot_reverse(left_speed, right_speed):
-    ALL_CONTROLLED[0].low()
-    ALL_CONTROLLED[1].high()
+    LEFT_MOTORS_PINS[0].low()
+    LEFT_MOTORS_PINS[1].high()
     
-    ALL_CONTROLLED[2].low()
-    ALL_CONTROLLED[3].high()
+    RIGHT_MOTORS_PINS[0].low()
+    RIGHT_MOTORS_PINS[1].high()
     
-    ALL_CONTROLLED[4].low()
-    ALL_CONTROLLED[5].high()
-    
-    ALL_CONTROLLED[6].low()
-    ALL_CONTROLLED[7].high()
+    if SPEED_PIN_LEFT:
+        SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+    if SPEED_PIN_RIGHT:
+        SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+
 
 def robot_stop():
-    ALL_CONTROLLED[0].low()
-    ALL_CONTROLLED[1].low()
+    LEFT_MOTORS_PINS[0].low()
+    LEFT_MOTORS_PINS[1].low()
     
-    ALL_CONTROLLED[2].low()
-    ALL_CONTROLLED[3].low()
-    
-    ALL_CONTROLLED[4].low()
-    ALL_CONTROLLED[5].low()
-    
-    ALL_CONTROLLED[6].low()
-    ALL_CONTROLLED[7].low()
+    RIGHT_MOTORS_PINS[0].low()
+    RIGHT_MOTORS_PINS[1].low()
     
 def robot_axis_right(left_speed, right_speed):
-    ALL_CONTROLLED[0].high()
-    ALL_CONTROLLED[1].low()
+    LEFT_MOTORS_PINS[0].high()
+    LEFT_MOTORS_PINS[1].low()
     
-    ALL_CONTROLLED[2].high()
-    ALL_CONTROLLED[3].low()
+    RIGHT_MOTORS_PINS[0].low()
+    RIGHT_MOTORS_PINS[1].high()
     
-    ALL_CONTROLLED[4].low()
-    ALL_CONTROLLED[5].high()
-    
-    ALL_CONTROLLED[6].low()
-    ALL_CONTROLLED[7].high()
+    if SPEED_PIN_LEFT:
+        SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+    if SPEED_PIN_RIGHT:
+        SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+
     
 def robot_axis_left(left_speed, right_speed):
-    ALL_CONTROLLED[0].low()
-    ALL_CONTROLLED[1].high()
+    LEFT_MOTORS_PINS[0].low()
+    LEFT_MOTORS_PINS[1].high()
     
-    ALL_CONTROLLED[2].low()
-    ALL_CONTROLLED[3].high()
-    
-    ALL_CONTROLLED[4].high()
-    ALL_CONTROLLED[5].low()
-    
-    ALL_CONTROLLED[6].high()
-    ALL_CONTROLLED[7].low()
+    RIGHT_MOTORS_PINS[0].high()
+    RIGHT_MOTORS_PINS[1].low()
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
-    
-def robot_slide_right(left_speed, right_speed):
-    ALL_CONTROLLED[0].low()
-    ALL_CONTROLLED[1].high()
-    
-    ALL_CONTROLLED[2].high()
-    ALL_CONTROLLED[3].low()
-    
-    ALL_CONTROLLED[4].high()
-    ALL_CONTROLLED[5].low()
-    
-    ALL_CONTROLLED[6].low()
-    ALL_CONTROLLED[7].high()
+    if SPEED_PIN_LEFT:
+        SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
-    
-def robot_slide_left(left_speed, right_speed):
-    ALL_CONTROLLED[0].high()
-    ALL_CONTROLLED[1].low()
-    
-    ALL_CONTROLLED[2].low()
-    ALL_CONTROLLED[3].high()
-    
-    ALL_CONTROLLED[4].low()
-    ALL_CONTROLLED[5].high()
-    
-    ALL_CONTROLLED[6].high()
-    ALL_CONTROLLED[7].low()
-
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+    if SPEED_PIN_RIGHT:
+        SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
     
 def run_left_motors_only(left_speed, right_speed):
-    ALL_CONTROLLED[0].high()
-    ALL_CONTROLLED[1].low()
+    LEFT_MOTORS_PINS[0].high()
+    LEFT_MOTORS_PINS[1].low()
     
-    ALL_CONTROLLED[2].high()
-    ALL_CONTROLLED[3].low()
+    RIGHT_MOTORS_PINS[0].low()
+    RIGHT_MOTORS_PINS[1].low()
     
-    ALL_CONTROLLED[4].low()
-    ALL_CONTROLLED[5].low()
-    
-    ALL_CONTROLLED[6].low()
-    ALL_CONTROLLED[7].low()
+    if SPEED_PIN_LEFT:
+        SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+    if SPEED_PIN_RIGHT:
+        SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+
     
 def run_right_motors_only(left_speed, right_speed):
-    ALL_CONTROLLED[0].low()
-    ALL_CONTROLLED[1].low()
+    LEFT_MOTORS_PINS[0].low()
+    LEFT_MOTORS_PINS[1].low()
     
-    ALL_CONTROLLED[2].low()
-    ALL_CONTROLLED[3].low()
-    
-    ALL_CONTROLLED[4].high()
-    ALL_CONTROLLED[5].low()
-    
-    ALL_CONTROLLED[6].high()
-    ALL_CONTROLLED[7].low()
+    RIGHT_MOTORS_PINS[0].high()
+    RIGHT_MOTORS_PINS[1].low()
 
-    SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
-    SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
-    
-def move_steer_1(angle): 
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[8])), angle, CONTROLLED_DEVICE_DETAILS[9], CONTROLLED_DEVICE_DETAILS[10])
-            
-def move_steer_2(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[9])), angle, CONTROLLED_DEVICE_DETAILS[12], CONTROLLED_DEVICE_DETAILS[13])
-            
-def move_shoulder_z(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[10])), angle, CONTROLLED_DEVICE_DETAILS[15], CONTROLLED_DEVICE_DETAILS[16])
-            
-def move_shoulder_x(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[11])), angle, CONTROLLED_DEVICE_DETAILS[18], CONTROLLED_DEVICE_DETAILS[19])
-            
-def move_elbow(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[12])), angle, CONTROLLED_DEVICE_DETAILS[21], CONTROLLED_DEVICE_DETAILS[22])
-            
-def move_wrist(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[13])), angle, CONTROLLED_DEVICE_DETAILS[24], CONTROLLED_DEVICE_DETAILS[25])
-            
-def move_gripper_1(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[14])), angle, CONTROLLED_DEVICE_DETAILS[27], CONTROLLED_DEVICE_DETAILS[28])
-            
-def move_gripper_2(angle):
-            set_servo_angle(machine.PWM(machine.Pin(ALL_CONTROLLED[15])), angle, CONTROLLED_DEVICE_DETAILS[30], CONTROLLED_DEVICE_DETAILS[31])
+    if SPEED_PIN_LEFT:
+        SPEED_PIN_LEFT.duty_u16(int(left_speed*65535))
 
+    if SPEED_PIN_RIGHT:
+        SPEED_PIN_RIGHT.duty_u16(int(right_speed*65535))
+
+    
+def move_servo(angle): 
+            set_servo_angle(machine.PWM(machine.Pin(SERVO_PINS[0])), angle, left_speed, right_speed)
+            
 def read_left_ir():
-    left_ir_value = ALL_CONTROLLERS[11].value()
+    left_ir_value = LEFT_IR_PINS[0].value()
     return left_ir_value
 
 def read_right_ir():
-    right_ir_value = ALL_CONTROLLERS[12].value()
+    right_ir_value = RIGHT_IR_PINS[0].value()
     return right_ir_value
 
 def obstacle_distance():
-    ALL_CONTROLLED[18].low()
+    TRIGGER_PINS[0].low()
     time.sleep_us(2)
-    ALL_CONTROLLED[18].high()
+    TRIGGER_PINS[0].high()
     time.sleep_us(10)
-    ALL_CONTROLLED[18].low()
+    TRIGGER_PINS[0].low()
 
-    pulse_duration = time_pulse_us(ALL_CONTROLLERS[13], 1, 30000)  # Wait for echo to go high (max 30ms)
+    pulse_duration = time_pulse_us(ECHO_PINS[0], 1, 30000)  # Wait for echo to go high (max 30ms)
     if pulse_duration < 0:
         return -1
 
